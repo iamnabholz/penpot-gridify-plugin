3f8514222ce27f75b890beb617818f5f837eca3c
Lukas Nabholz <lukas@nabholz.work>
2024-11-19 20:37:26 -0400

    Updated icon

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,29 @@ import "./style.css";
 const searchParams = new URLSearchParams(window.location.search);
 document.body.dataset.theme = searchParams.get("theme") ?? "light";
 
-document.querySelector("[data-handler='create-text']")?.addEventListener("click", () => {
-  // send message to plugin.ts
-  parent.postMessage("create-text", "*");
+const choiceSelector = document.querySelector(".choice-selector")!;
+let selectedValue = "Dots";
+
+choiceSelector.addEventListener("click", (event: Event) => {
+  const target = event.target as HTMLElement;
+
+  if (target.tagName === "P") {
+    // Remove current selection
+    const currentSelected = choiceSelector.querySelector(".choice-selected");
+    currentSelected?.classList.remove("choice-selected");
+
+    // Add selection to clicked item
+    target.classList.add("choice-selected");
+    selectedValue = target.textContent || "Any";
+  }
 });
+
+document
+  .querySelector("[data-handler='create-text']")
+  ?.addEventListener("click", () => {
+    // send message to plugin.ts
+    parent.postMessage("create-text", "*");
+  });
 
 // Listen plugin.ts messages
 window.addEventListener("message", (event) => {
